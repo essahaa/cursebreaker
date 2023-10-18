dcdbd86d5b45e626abf2c344009d964f38284bfe
Rikulis <remesrikua@gmail.com>
2023-10-18 13:53:38 +0300

    Added: LevelSelection Scene
    New scene for level selection

--- CurseBreaker/Assets/Scripts/MainMenu.cs
+++ CurseBreaker/Assets/Scripts/MainMenu.cs
@@ -13,8 +13,8 @@ public class MainMenu : MonoBehaviour
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void Quit()
+    public void LevelSelection()
     {
-        Application.Quit();
+        SceneManager.LoadScene("LevelSelection");
     }
 }
