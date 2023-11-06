debf932e67cf8434bd4675e1979f8ed75430c652
Rikulis <remesrikua@gmail.com>
2023-11-06 12:48:47 +0200

    Updated: Continue button on tutorial level

--- CurseBreaker/Assets/Scripts/MainMenu.cs
+++ CurseBreaker/Assets/Scripts/MainMenu.cs
@@ -13,6 +13,13 @@ public class MainMenu : MonoBehaviour
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Gameboard()
+    {
+        SceneManager.LoadScene("Gameboard");
+
+   
+    }
+
     public void LevelSelection()
     {
         SceneManager.LoadScene("LevelSelection");
