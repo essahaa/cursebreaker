c0ee350cfd4fd02db1f2f6f8942f26ced9e7f2e7
Rikulis <remesrikua@gmail.com>
2023-11-14 15:08:21 +0200

    Added: Loadingscreen (doesnt work)

---
+++ CurseBreaker/Assets/Scripts/ASyncLoader.cs
@@ -0,0 +1,35 @@
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ASyncLoader : MonoBehaviour
+{
+    [Header("Menu Screens")]
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject mainMenu;
+
+    [Header("Slider")]
+    [SerializeField] private Slider loadingSlider;
+
+    public void LoadLevelBtn(string levelToLoad)  // Removed space in method name
+    {
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadLevelAsync(levelToLoad));
+    }
+
+    IEnumerator LoadLevelAsync(string levelToLoad)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
+
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingSlider.value = progressValue;
+            yield return null;
+        }
+    }
+}
