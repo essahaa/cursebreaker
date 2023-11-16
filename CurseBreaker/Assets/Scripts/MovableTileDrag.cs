5f7ecea49bb9b4b669f239bafa72825906f16afd
pinjaa <91983754+pinjaa@users.noreply.github.com>
2023-11-16 13:15:13 +0200

    issnappedtonewplace korjattu

--- CurseBreaker/Assets/Scripts/MovableTileDrag.cs
+++ CurseBreaker/Assets/Scripts/MovableTileDrag.cs
@@ -247,52 +247,56 @@ public class MovableTileDrag : MonoBehaviour
                         }
                     }
                 }
-
             }
             //empty the currently moved row or column from movabletiles array so that it can be replaced by new positions
             movableTiles = (currentMoveType == "horizontal") ? movableTileGrid.FindAllMovableTilesInRow(rowIndex) : movableTileGrid.FindAllMovableTilesInColumn(columnIndex);
             movableTileGrid.EmptyMovableTilesArrayRowOrColumn(movableTiles);
 
-            // Snap each tile to the nearest grid position based on rows and columns.
-            foreach (Transform tile in currentMovableTiles)
+            for (int row = 0; row < currentMovableTiles.GetLength(0); row++)
             {
-                if (tile != null)
+                for (int col = 0; col < currentMovableTiles.GetLength(1); col++)
                 {
-                    MovableTile movableTileComponent = tile.GetComponent<MovableTile>();
+                    Transform tile = currentMovableTiles[col, row];
 
-                    // Skip the locked tiles
-                    if (movableTileComponent.IsLocked)
-                    {
-                        Debug.Log($"Locked Tile Final Position at [{movableTileComponent.Column}, {movableTileComponent.Row}]: {tile.position}");
-                        continue;
-                    }
+                    if (tile != null)
+                    {                      
+                        MovableTile movableTileComponent = tile.GetComponent<MovableTile>();
 
-                    // Calculate the target snapped position based on row and column.
-                    float targetX = movableTileComponent.Column * backgroundGrid.backgroundTileSize + backgroundGrid.minX;
-                    float targetY = movableTileComponent.Row * backgroundGrid.backgroundTileSize + backgroundGrid.minY;
+                        // Skip the locked tiles
+                        if (movableTileComponent.IsLocked)
+                        {
+                            Debug.Log($"Locked Tile Final Position at [{movableTileComponent.Column}, {movableTileComponent.Row}]: {tile.position}");
+                            continue;
+                        }
 
-                    // Ensure the snapped position stays within the background grid boundaries.
-                    targetX = Mathf.Clamp(targetX, backgroundGrid.minX, backgroundGrid.maxX);
-                    targetY = Mathf.Clamp(targetY, backgroundGrid.minY, backgroundGrid.maxY);
+                        // Calculate the target snapped position based on row and column.
+                        float targetX = movableTileComponent.Column * backgroundGrid.backgroundTileSize + backgroundGrid.minX;
+                        float targetY = movableTileComponent.Row * backgroundGrid.backgroundTileSize + backgroundGrid.minY;
 
-                    // Set the tile's position to the target position.
-                    tile.position = new Vector3(targetX, targetY, 0f);
+                        // Ensure the snapped position stays within the background grid boundaries.
+                        targetX = Mathf.Clamp(targetX, backgroundGrid.minX, backgroundGrid.maxX);
+                        targetY = Mathf.Clamp(targetY, backgroundGrid.minY, backgroundGrid.maxY);
 
-                    // Update the movableTileGrid with the new position.
-                    movableTileGrid.UpdateMovableTile(movableTileComponent.Column, movableTileComponent.Row, tile);
-                    Debug.Log("Snapped to row: " + movableTileComponent.Row + ", column: " + movableTileComponent.Column + " tile position " + tile.position);
+                        // Set the tile's position to the target position.
+                        tile.position = new Vector3(targetX, targetY, 0f);
 
-                    if (!tileInSamePosition)
-                    {
-                        isSnappedToNewPlace = true;
-                    }
-                    else
-                    {
-                        isSnappedToNewPlace = false;
+                        // Update the movableTileGrid with the new position.
+                        movableTileGrid.UpdateMovableTile(movableTileComponent.Column, movableTileComponent.Row, tile);
+                        Debug.Log("Snapped to row: " + movableTileComponent.Row + ", column: " + movableTileComponent.Column + " tile position " + tile.position);
+
+                        if (tile.position == initialTilePositions[col, row])
+                        {
+                            isSnappedToNewPlace = false;
+                        }
+                        else
+                        {
+                            isSnappedToNewPlace = true;
+                        }
+                        Debug.Log("issnappedtonewplace " + isSnappedToNewPlace);
                     }
                 }
             }
-
+            
             movableTileGrid.DestroyKeyAndLockTilesIfNeighbor();
 
             if (isSnappedToNewPlace)
@@ -329,7 +333,7 @@ public class MovableTileDrag : MonoBehaviour
                     }
                 }
             }
-            allElementsNull = false;
+            allElementsNull = true;
             tileInSamePosition = false;
         }
 
