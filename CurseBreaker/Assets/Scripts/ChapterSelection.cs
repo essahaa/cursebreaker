827eacb7a5cf35280bb84d79c7bc43d7987774a2
Oivoi1 <n1oive00@students.oamk.fi>
2023-11-28 13:25:56 +0200

    Nyt tulee tähdet levelien perusteella
    Vielä pitää päivitys tehdä ja visuaalista puolta korjailla.

--- CurseBreaker/Assets/Scripts/ChapterSelection.cs
+++ CurseBreaker/Assets/Scripts/ChapterSelection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Add this at the top of your script
 
 public class ChapterSelectionController : MonoBehaviour
 {
@@ -23,12 +24,27 @@ public class ChapterSelectionController : MonoBehaviour
         for (int i = 0; i < levelButtons.Length; i++)
         {
             Transform starPlaceholder = levelButtons[i].transform.Find("StarPlaceholder");
-            if (starPlaceholder != null)
+
+            TextMeshProUGUI levelText = levelButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            if (levelText != null)
             {
-                GameObject starDisplay = Instantiate(starDisplayPrefab, starPlaceholder.position, Quaternion.identity, starPlaceholder);
-                int starsEarned = PlayerPrefs.GetInt("Level_" + i + "_Stars", 0);
-                starDisplay.GetComponent<StarDisplayUpdater>().UpdateStars(starsEarned);
-                Debug.Log("Tän pitäis toimii?");
+                if (int.TryParse(levelText.text, out int levelNumber))
+                {
+                    if (starPlaceholder != null)
+                    {
+                        GameObject starDisplay = Instantiate(starDisplayPrefab, starPlaceholder.position, Quaternion.identity, starPlaceholder);
+                        int starsEarned = PlayerPrefs.GetInt("Level_" + levelNumber + "_Stars", 0);
+                        starDisplay.GetComponent<StarDisplayUpdater>().UpdateStars(starsEarned);
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Failed to parse level number from TextMeshPro on button: " + i);
+                }
+            }
+            else
+            {
+                Debug.LogError("TextMeshPro component not found on button: " + i);
             }
         }
     }
