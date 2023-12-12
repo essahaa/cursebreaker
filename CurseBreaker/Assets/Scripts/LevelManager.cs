a7076969ec4febae4fd3b371dd3869935c35e805
booennm <booennm@gmail.com>
2023-12-12 17:54:09 +0200

    dialog comes up more

--- CurseBreaker/Assets/Scripts/LevelManager.cs
+++ CurseBreaker/Assets/Scripts/LevelManager.cs
@@ -340,6 +340,7 @@ public class LevelManager : MonoBehaviour
                 {
                     case 1:
                         dialogBubbleImage.sprite = bubbleRight;
+                        animator.SetTrigger("LiftCharacters");
                         tmp.text = dialogue1_1;
                         break;
                     case 2:
@@ -371,6 +372,7 @@ public class LevelManager : MonoBehaviour
                 {
                     case 1:
                         dialogBubbleImage.sprite = bubbleRight;
+                        animator.SetTrigger("LiftCharacters");
                         tmp.text = dialogue2_1;
                         break;
                     case 2:
@@ -402,21 +404,22 @@ public class LevelManager : MonoBehaviour
                 {
                     case 1:
                         dialogBubbleImage.sprite = bubbleRight;
-                        tmp.text = dialogue2_1;
+                        animator.SetTrigger("LiftCharacters");
+                        tmp.text = dialogue3_1;
                         break;
                     case 2:
-                        tmp.text = dialogue2_2;
+                        tmp.text = dialogue3_2;
                         break;
                     case 3:
                         tmp.text = "";
                         animator.SetBool("CharacterDown", true);
                         GetSideCharacter();
                         animator.SetBool("CharacterDown", false);
-                        tmp.text = dialogue2_3;
+                        tmp.text = dialogue3_3;
                         break;
                     case 4:
                         dialogBubbleImage.sprite = bubbleLeft;
-                        tmp.text = dialogue2_4;
+                        tmp.text = dialogue3_4;
                         break;
                     default:
                         tmp.text = "";
@@ -433,10 +436,11 @@ public class LevelManager : MonoBehaviour
                 {
                     case 1:
                         dialogBubbleImage.sprite = bubbleRight;
-                        tmp.text = dialogue2_1;
+                        animator.SetTrigger("LiftCharacters");
+                        tmp.text = dialogue4_1;
                         break;
                     case 2:
-                        tmp.text = dialogue2_2;
+                        tmp.text = dialogue4_2;
                         break;
                     default:
                         tmp.text = "";
